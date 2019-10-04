668b8b7857eb8d1d349b24cdcab4dc65b7c83796
LoretaShala <39461219+LoretaShala@users.noreply.github.com>
2019-10-05 00:58:30 +0200

    Add files via upload

---
+++ StringKey.java
@@ -0,0 +1,24 @@
+public class StringKey implements Key
+{
+    private String id;  // the key
+
+    public StringKey(String j)
+    {
+        id = j;
+    }
+
+    public String valOf()
+    {
+        return id;
+    }
+
+    public boolean equals(Key another_key)
+    {
+        boolean answer;
+        String m = ((StringKey)another_key).valOf();
+        answer = (id.equals(m));
+        return answer;
+    }
+
+   
+}
